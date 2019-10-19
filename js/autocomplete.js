00eb080ea84e9fa6cdcac7cfe0f637e8574d9222
Markus Wase <markuswase@hotmail.com>
2019-10-19 10:32:49 +0200

    * Added a few explanatory comments
    * Added an a-tag around the content in the list-item for autocomplete to be able to tab and select without using the mouse.

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -10,9 +10,12 @@ var Autocomplete = (function() {
             clearTimeout(searchTimeoutId);
         }
 
+        // Prevent the server from being overloaded with requests, so wait for SEARCH_TIMEOUT_MS ms to make sure
+        // no more input is given. Restart the timeout every time a new request is made.
         searchTimeoutId = setTimeout(async function() {
             var wordList = [];
 
+            // Sanitize input to prevent query string manipulation
             word = word.replace(/[^a-zA-Z ]/g, '');
 
             if (word) {
--- js/search-area.js
+++ js/search-area.js
@@ -12,11 +12,12 @@
         searchInput.focus();
     });
 
-    searchInput.addEventListener('keyup', function(event) {
+    searchInput.addEventListener('keyup', function() {
         Autocomplete.searchWord(this.value, updateResults);
     });
 
     clearHistoryLink.addEventListener('click', function() {
+        // Clear the history list
         historyList.innerHTML = '';
     });
 
@@ -25,27 +26,35 @@
     }
 
     function updateAutocompleteResults(wordList, inputWord) {
+        // Clear data from the autocomplete lists and show if there are items in the wordList-property
         searchResultsContainer.innerHTML = '';
         searchResultsWrapper.classList.toggle('search-area__autocomplete-results--active', wordList.length > 0);
 
         for (var index in wordList) {
             var word = wordList[index].word;
 
+            // Create the list item element
             var dummyListItem = document.createElement('li');
             dummyListItem.classList = 'search-area__autocomplete-results__results-list__item';
 
+            // Create the highlight span for the list item
             var dummyListItemHightlightSpan = document.createElement('span');
             dummyListItemHightlightSpan.classList = 'search-area__autocomplete-results__results-list__item__highlighted';
             dummyListItemHightlightSpan.innerText = inputWord;
 
+            var dummyListItemLink = document.createElement('a');
+            dummyListItemLink.href = '#';
+
+            // Sanitize the string that goes into innerHTML to prevent XSS or disruptive HTML
             var sanitizedWord = dummyListItemHightlightSpan.innerText;
-
             var wordWithSplit = word.replace(sanitizedWord, dummyListItemHightlightSpan.outerHTML);
-            dummyListItem.innerHTML = wordWithSplit;
+            dummyListItemLink.innerHTML = wordWithSplit;
 
-            dummyListItem.addEventListener('click', function() {
+            dummyListItemLink.addEventListener('click', function() {
                 updateHistory(this.innerText);
             });
+
+            dummyListItem.appendChild(dummyListItemLink);
 
             searchResultsContainer.appendChild(dummyListItem);
         }
@@ -54,13 +63,17 @@
     function updateHistory(inputWord) {
         if (inputWord.length) {
             var timestamp = Helper.formattedTimestamp();
+
+            // Create history list item element
             var historyChildItem = document.createElement('li');
             historyChildItem.classList = 'search-area__history__list__item';
     
+            // Create the timestamp span for the list item
             var historyChildTimestampSpan = document.createElement('span');
             historyChildTimestampSpan.classList = 'search-area__history__list__item__timestamp';
             historyChildTimestampSpan.innerText = timestamp;
     
+            // Create the removal link for the list item
             var historyChildRemoveLink = document.createElement('a');
             historyChildRemoveLink.classList = 'search-area__history__list__item__remove';
             historyChildRemoveLink.innerText = '🞩';
@@ -70,6 +83,7 @@
             });
     
             historyChildItem.innerText = inputWord;
+
             historyChildItem.appendChild(historyChildRemoveLink);
             historyChildItem.appendChild(historyChildTimestampSpan);
     
